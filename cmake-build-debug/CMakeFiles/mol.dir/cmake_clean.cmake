25de117c6fb2c30616593e97f940173d2d9d2f57
Mike McDermott <mikemcdermott23@gmail.com>
2016-12-19 10:22:25 -0500

    unpatched speed clip check

---
+++ cmake-build-debug/CMakeFiles/mol.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mol.dir/main.cpp.o"
+  "CMakeFiles/mol.dir/rng.cpp.o"
+  "CMakeFiles/mol.dir/main.c.o"
+  "CMakeFiles/mol.dir/aux.c.o"
+  "CMakeFiles/mol.dir/xspeedtest.c.o"
+  "CMakeFiles/mol.dir/argparse.c.o"
+  "mol.pdb"
+  "mol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/mol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
